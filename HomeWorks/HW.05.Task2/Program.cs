c427863d384458e2de1c7dc5fbcd075df3594180
lfid95 <lfid95@gmail.com>
2021-02-20 16:48:45 +0300

    Added: HomeWork 05

---
+++ HomeWorks/HW.05.Task2/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace HW._05.Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        link1:
+            Console.WriteLine("Enter the number above 0 and below 100:");
+            string numTxt = Console.ReadLine();
+            int num;
+            while (Program.NumCheck(numTxt) == false)
+            {
+                Console.WriteLine("Enter the number above 0 and below 100:");
+                numTxt = Console.ReadLine();
+                num = Convert.ToInt32(numTxt);
+            }
+            num = Convert.ToInt32(numTxt);
+            if (num >= 100) goto link1;
+            int x = num;
+            for (int i = 1; i <= num; i++)
+            {
+                int[] numPiramid = new int[x];
+                x--;
+                foreach (int n in numPiramid)
+                {
+                    Console.Write(i%10 + " ");
+                }
+                Console.WriteLine();
+                for(int z = 1; z <= i; z++)
+                {
+                    Console.Write(" ");
+                }
+            }
+        }
+        static bool NumCheck(string s)
+        {
+            for (int i = 0; i < s.Length; i++)
+                if (!char.IsDigit(s[i])) return false;
+            return true;
+        }
+    }
+}
---
+++ HomeWorks/HW.05.Task4/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace HW._05.Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Console.WriteLine("Enter the word:");
+            string word = Console.ReadLine();
+            int wordL = word.Length - 1;
+            int x = 0;
+            for (int i = 0; i <= wordL; i++)
+            {
+                char a = 'a';
+                char bigA = 'A';
+                char aKirr = 'а';
+                char aKirrBig = 'А';
+                if (word[i] == a || word[i] == bigA || word[i] == aKirr || word[i] == aKirrBig)
+                {
+                    x++;
+                }
+            }
+            Console.WriteLine("There are " + x + " leters 'a' in this word.");
+            
+        }
+    }
+}
