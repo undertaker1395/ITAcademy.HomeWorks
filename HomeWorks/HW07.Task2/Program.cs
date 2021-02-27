81f9c7787d6107c102c93e1dbfdbbe93820455ac
lfid95 <lfid95@gmail.com>
2021-02-27 23:46:08 +0300

    Edded 1/4 task2

---
+++ HomeWorks/HW07.Task2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace HW07.Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        public string NoLongWord(string newStr)
+        {
+            string[] strArr = newStr.Split(' ');
+            string maxLWord = string.Empty;
+            for (int i = 0; i < strArr.Length; i++)
+                if (strArr[i].Length > maxLWord.Length)
+                    maxLWord = strArr[i];
+            for (int i = 0; i < strArr.Length; i++)
+                if (strArr[i] == maxLWord)
+                    strArr[i] = null;
+            return string.Join(' ', strArr);
+        }
+    }
+}
