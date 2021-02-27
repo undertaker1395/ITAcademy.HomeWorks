f9725a8287d17f50e70654f522d7da0fd3c71f6a
lfid95 <lfid95@gmail.com>
2021-02-27 23:05:53 +0300

    HW07: added Task1

---
+++ HomeWorks/HW07.Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HW07.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter poem. Use \";\" to split strings:");
+            string poemO = Console.ReadLine();
+            string poemA = poemO.Replace('O', 'A');
+            string[] poemSplit = poemA.Split(';');
+            foreach (string newStrPoem in poemSplit)
+            {
+                Console.WriteLine(newStrPoem);
+            }
+        }
+    }
+}
