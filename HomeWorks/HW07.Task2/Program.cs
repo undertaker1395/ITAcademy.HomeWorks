ac78c9a35469087d4cbe27103850cc023967051e
lfid95 <lfid95@gmail.com>
2021-02-28 14:30:35 +0300

    added 3/4 task2

--- HomeWorks/HW07.Task2/Program.cs
+++ HomeWorks/HW07.Task2/Program.cs
@@ -6,20 +6,100 @@ namespace HW07.Task2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Enter string:");
+            string inputStr = Console.ReadLine();
+            Console.WriteLine(Program.NoLongWord(inputStr));
+            Console.WriteLine(Program.LongShortExch(inputStr));
+            Console.WriteLine(Program.FromSToL(inputStr));
         }
 
-        public string NoLongWord(string newStr)
+        static string NoLongWord(string newStr)
         {
             string[] strArr = newStr.Split(' ');
             string maxLWord = string.Empty;
+            int count = 0;
             for (int i = 0; i < strArr.Length; i++)
                 if (strArr[i].Length > maxLWord.Length)
+                {
                     maxLWord = strArr[i];
+                    count = i;
+                }
+            string[] newArr = new string[strArr.Length - 1];
+            for (int i = 0; i < count; i++)
+            {
+                newArr[i] = strArr[i];
+            }
+            for (int i = count; i < newArr.Length; i++)
+            {
+                newArr[i] = strArr[i+1];
+            }
+            return string.Join(' ', newArr);
+        }
+
+        static string LongShortExch(string newStr)
+        {
+            string[] strArr = newStr.Split(' ');
+            string maxLWord = string.Empty;
+            string minLWord = string.Empty;
+            int indexL = 0;
+            int indexS = 0;
             for (int i = 0; i < strArr.Length; i++)
-                if (strArr[i] == maxLWord)
-                    strArr[i] = null;
+                if (strArr[i].Length > maxLWord.Length)
+                {
+                    maxLWord = strArr[i];
+                    indexL = i;
+                }
+            minLWord = maxLWord;
+            for (int i = 0; i < strArr.Length; i++)
+
+                if (strArr[i].Length < minLWord.Length)
+                {
+                    minLWord = strArr[i];
+                    indexS = i;
+                }
+            string[] newArr = new string[strArr.Length];
+            for (int i = 0; i < newArr.Length; i++)
+            {
+                if (indexL == i)
+                {
+                    newArr[i] = minLWord;
+                }
+                else
+                {
+                    if (indexS == i)
+                    {
+                        newArr[i] = maxLWord;
+                    }
+                    else
+                    {
+                        newArr[i] = strArr[i];
+                    }
+                }
+            }
+            
+            return string.Join(' ', newArr); 
+        }
+        static string FromSToL(string newStr)
+        {
+            string[] strArr = newStr.Split(' ');
+            string word;
+            for (int i = 0; i < newStr.Length - 1; i++)
+            {
+                for (int j = i + 1; j < strArr.Length; j++)
+                {
+                    if (strArr[i].Length > strArr[j].Length)
+                    {
+                        word = strArr[i];
+                        strArr[i] = strArr[j];
+                        strArr[j] = word;
+                    }
+                }
+            }
             return string.Join(' ', strArr);
+        }
+        static string LeterCount(string newStr)
+        {
+            char[] byLeters = newStr.ToCharArray();
         }
     }
 }
