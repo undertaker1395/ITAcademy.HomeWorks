837db1c0a7c8b43b0d1fc63c2b42b97705dab49d
lfid95 <lfid95@gmail.com>
2021-03-02 21:18:06 +0300

    Added Task3

--- HomeWorks/AssemblyOne/Motorcycle.cs
+++ HomeWorks/AssemblyOne/Motorcycle.cs
@@ -4,12 +4,12 @@ using System.Text;
 
 namespace AssemblyOne
 {
-    class Motorcycle
+    public class Motorcycle
     {
         const ushort speed = 420;
-        string vinNumer = "ASD76678-AS76F-S8D7F6G-SD78GF";
-        int odometr = 69_000;
-
+        static string vinNumer = "ASD76678-AS76F-S8D7F6G-SD78GF";
+        static int odometr = 69_000;
+        
         void StartEngine()
         {
 
@@ -22,6 +22,43 @@ namespace AssemblyOne
         protected internal ushort speedProtIntern = speed;
         private protected ushort speedPrivProt = speed;
 
-        
+        public string vinNumerPublic = vinNumer;
+        private string vinNumerPrivate = vinNumer;
+        protected string vinNumerProtected = vinNumer;
+        internal string vinNumerInternal = vinNumer;
+        protected internal string vinNumerProtIntern = vinNumer;
+        private protected string vinNumerPrivProt = vinNumer;
+
+        public int odometrPublic = odometr;
+        private int odometrPrivate = odometr;
+        protected int odometrProtected = odometr;
+        internal int odometrInternal = odometr;
+        protected internal int odometrProtIntern = odometr;
+        private protected int odometrPrivProt = odometr;
+
+        public void StartEnginePublic()
+        {
+            StartEngine();
+        }
+        private void StartEnginePrivate()
+        {
+            StartEngine();
+        }
+        protected void StartEngineProtected()
+        {
+            StartEngine();
+        }
+        internal void StartEngineInternal()
+        {
+            StartEngine();
+        }
+        protected internal void StartEngineProtIntern ()
+        {
+            StartEngine();
+        }
+        private protected void StartEnginePrivProt()
+        {
+            StartEngine();
+        }
     }
 }
--- HomeWorks/AssemblyOne/Program.cs
+++ HomeWorks/AssemblyOne/Program.cs
@@ -9,25 +9,44 @@ namespace AssemblyOne
             Motorcycle moto = new Motorcycle();
 
             moto.speedPublic = 100;
-
-            //имеет модификатор private и класс Program ее не видит
-            //moto.speedPrivate = 100; Ошибка, доступ ограничен
-
-            //класс Program не является классом-наследником класса Motocycle
-            //moto.speedProtected = 100; Ошибка, доступ ограничен
-
+            //moto.speedPrivate = 100; Ошибка, доступ ограничен. имеет модификатор private и класс Program ее не видит
+            //moto.speedProtected = 100; Ошибка, доступ ограничен. класс Program не является классом-наследником класса Motocycle
             moto.speedInternal = 100;
             moto.speedProtIntern = 100;
+            //moto.speedPrivProt = 100; Ошибка, доступ ограничен. класс Program не является классом-наследником класса Motocycle
 
-            //класс Program не является классом-наследником класса Motocycle
-            //moto.speedPrivProt = 100; Ошибка, доступ ограничен
+            moto.odometrInternal = 70_000;
+            moto.odometrProtIntern = 70_000;
+            moto.odometrPublic = 70_000;
 
+            moto.StartEngineInternal();
+            moto.StartEngineProtIntern();
+            moto.StartEnginePublic();
 
+            moto.vinNumerInternal = "ASHDGJHG-37486T-SFJFKSJD";
+            moto.vinNumerProtIntern = "hecfwuic-crwe-3c244c";
+            moto.vinNumerPublic = "324ghjg-345jkhjghj5-243g";
         }
     }
 
     class SportBike : Motorcycle
     {
+        static void Main(string[] args)
+        {
+            SportBike sMoto = new SportBike();
+            sMoto.speedPublic = 120;
+            //moto.speedPrivate = 100; Ошибка, доступ ограничен. имеет модификатор private и класс SportBike ее не видит
+            sMoto.speedInternal = 120;
+            sMoto.speedPrivProt = 120;
+            sMoto.speedProtected = 120;
+            sMoto.speedProtIntern = 120;
 
+            sMoto.StartEngineInternal();
+            sMoto.StartEnginePrivProt();
+            sMoto.StartEngineProtected();
+            sMoto.StartEngineProtIntern();
+            sMoto.StartEnginePublic();
+            
+        }
     }
 }
